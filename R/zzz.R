d7d75a0ad25b58a6759fbdf9c079114b7c669765
selvastics <c.selva@outlook.de>
2025-09-04 12:53:04 +0200

    Merge namespace imports/globals into zzz.R and remove duplicate file

--- R/zzz.R
+++ R/zzz.R
@@ -5,6 +5,22 @@
 #' @name zzz
 #' @keywords internal
 
+#' Internal imports and globals
+#' @keywords internal
+#' @importFrom graphics grid polygon
+#' @importFrom utils globalVariables
+NULL
+
+utils::globalVariables(c(
+  ".active_sessions", ".log_path", ".SD", ".webdav_config",
+  "display_enhanced_llm_prompt", "export_csv", "export_json", "export_r",
+  "export_spss", "export_xlsx", "generate_task_specific_prompt",
+  "get_participant", "grid", "Item", "list_participants", "polygon",
+  "print.inrep_config", "print.professional_survey", "remove_participant",
+  "SE", "send_invitation", "send_reminder", "Theta", "update_participant"
+))
+
+
 # Package namespace environment
 .inrep_env <- new.env(parent = emptyenv())
 
